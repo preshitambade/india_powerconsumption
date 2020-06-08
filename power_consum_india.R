7e953ac069f6a5873e15830c62d4be93122b490a
Preshit Ambade <preshitambade@gmail.com>
2020-06-08 15:19:55 -0700

    test

---
+++ power_consum_india.R
@@ -0,0 +1,8 @@
+# India Power Consumption during COVID-19 period
+# preshit ambade
+#preshitambade@gmail.com
+# date: 8th June 2020
+# Data source:  {https://www.kaggle.com/twinkle0705/state-wise-power-consumption-in-india}
+# Animation inspiration:{https://kelseygonzalez.github.io/portfolio/COVID-19%20Population%20Mobility/}
+
+
